ee1de9d43ae3e547c6e5a8a6d86c0c28204ec699
przemek978 <przemek.ku2000@gmail.com>
2023-01-03 11:56:24 +0100

    Dodaj pliki projektów.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntiveApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
